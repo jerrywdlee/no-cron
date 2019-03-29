119a1cf1e4691661606bbf5eae7d6c1e87680d94
Jerry Lee <lee@idoctor.co.jp>
2019-03-30 00:39:32 +0900

    update cmd

--- README.md
+++ README.md
@@ -3,11 +3,21 @@ A node.js based fake cron.
 
 ## Usage
 
+### Run in cli
+
 ```sh
 # Need node.js v8+
 $ npm install
 $ node cron # This will execute cronjobs in system crontab
-$ node cron my_cron.txt # Execute cronjobs wrote in a file called `my_cron.txt`
+$ node cron -f my_cron.txt # Execute cronjobs wrote in a file called `my_cron.txt`
+```
+### Run as daemon
+
+```sh
+$ npm i && npm i -g pm2
+$ pm2 start cron.js
+# OR
+$ pm2 start cron.js -- -f my_cron.txt
 ```
 
 ## Known issues
--- cron.js
+++ cron.js
@@ -5,11 +5,12 @@ const { exec, execSync } = require('child_process')
 const { existsSync, readFileSync } = require('fs')
 const { promisify } = require('util')
 const exa = promisify(exec)
+const argv = require('./lib/argv')
 
 let cronText = ''
 
-if (process.argv[2]) {
-  const fileName = process.argv[2]
+if (argv.file) {
+  const fileName = argv.file
   if (existsSync(fileName)) {
     cronText = readFileSync(fileName, 'utf8')
   } else {
@@ -47,7 +48,7 @@ cronText.split(/\r\n|\r|\n/).forEach(line => {
 const jobs = cronJobs.map(cronJob => {
   const [cond, cmd] = cronJob
   if (cond && cmd) {
-    // DEBUG:
+    // FOR DEBUG:
     console.log(cond, cmd)
     return new CronJob(cond, async () => {
       try {
@@ -65,6 +66,7 @@ if (jobs.length > 0) {
   console.log(`${jobs.length} cron job(s) found, cron started.`)
 } else {
   console.log('No cron job found!')
+  process.exit(2)
 }
 
 function parseCronLine(cronTimeStr, line) {
