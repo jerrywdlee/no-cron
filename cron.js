61dea191035bcdf3c16f3fd7d3d8b300a814223c
Jerry Lee <lee@idoctor.co.jp>
2019-04-07 17:53:43 +0900

    add License into file heads

--- cron.js
+++ cron.js
@@ -1,3 +1,19 @@
+/**
+ * Copyright (c) 2019 Jerry Lee <jerrywdlee@gmail.com>
+ *
+ * This project is dual licensed under
+ * Anti 996 License Version 1.0 & Mozilla Public License 2.0
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * See Anti 996 License Version 1.0 Here:
+ * https://github.com/jerrywdlee/no-cron/blob/master/LICENSE#L5
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * See Mozilla Public License 2.0 Here:
+ * https://github.com/jerrywdlee/no-cron/blob/master/LICENSE#L52
+ *
+ */
+
 'use strict'
 
 const { CronJob, CronTime } = require('cron')
